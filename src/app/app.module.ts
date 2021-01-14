64a11aa8f3a7225a5e10c74baaabff3cd26324f2
lukeman.kamara <l.kamara@ubuntuafrika.com>
2021-01-14 10:51:42 -0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeUpdateComponent } from './employee-update/employee-update.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeeListComponent,
+    EmployeeUpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employee.ts
@@ -0,0 +1,11 @@
+export class Employee {
+  id: number;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  address: string;
+  dateOfBirth: Date;
+  department: string;
+  emailId: string;
+  active: boolean;
+}
