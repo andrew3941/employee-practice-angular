c99bb23cbcc28f2bf4ac3fd2865d7da591c53e92
Musa B. Sesay <musasesay1771@gmail.com>
2021-01-25 10:55:57 +0000

    CREATE UI for Employes Table

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor() { }
+  constructor(  private router: Router) {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  // this is to navigate update
+  goUpdate() {
+    this.router.navigate(['/update/1']);
+  }
+
+  details() {
+    this.router.navigate(['/details/1']);
+  }
+
+  deleteEmployee(id: number) {
+    // this.employeeService.deleteEmployee(id)
+    //   .subscribe(
+    //     data => {
+    //       console.log(data);
+    //       this.reloadData();
+    //     },
+    //     error => console.log(error));
+  }
+
+  employeeDetails(id: number){
+    // this.router.navigate(['details', id]);
+  }
+
+  updateEmployee(id: number){
+    // this.router.navigate(['update', id]);
   }
 
 }
